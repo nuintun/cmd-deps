376ca661ebf4ed3181565ff6bbad7d24289c7dca
Nuintun <nuintun@qq.com>
2015-05-05 00:06:31 +0800

    update to 0.0.3

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -3,17 +3,17 @@ var detective = require('detective');
 var Benchmark = require('benchmark');
 
 var tests = {
-  'normal': 'require("a");require(\'b"\');require("c\\"");function(require){return require;}',
-  'reg & comment': '(1)/*\n*/ / require("a");function(require){return require;}',
-  'after return': "return require('highlight.js').highlightAuto(code).value;function(require){return require;}",
-  'in quote': '"require(\'a\')";function(require){return require;}',
-  'in comment': 'require("a");//require("a");function(require){return require;}',
-  'in multi comment': '/*\nrequire("a")*/require("a");function(require){return require;}',
-  'in reg': '/require("a")/;function(require){return require;}',
-  'in ifstmt with no {}': 'if(true)/require("a")/;function(require){return require;}',
-  'in dostmt with no {}': 'do /require("a")/.test(s); while(false);function(require){return require;}',
-  'reg / reg': '/require("a")/ / /require("b");function(require){return require;}',
-  'ignore variable': 'require("a" + b);function(require){return require;}'
+  'normal': 'require("a");require(\'b"\');require("c\\"");function require(require){return require;}',
+  'reg & comment': '(1)/*\n*/ / require("a");function require(require){return require;}',
+  'after return': "return require('highlight.js').highlightAuto(code).value;function require(require){return require;}",
+  'in quote': '"require(\'a\')";function require(require){return require;}',
+  'in comment': 'require("a");function require(require){return require;}//require("a");',
+  'in multi comment': '/*\nrequire("a")*/require("a");function require(require){return require;}',
+  'in reg': '/require("a")/;function require(require){return require;}',
+  'in ifstmt with no {}': 'if(true)/require("a")/;function require(require){return require;}',
+  'in dostmt with no {}': 'do /require("a")/.test(s); while(false);function require(require){return require;}',
+  'reg / reg': '/require("a")/ / /require("b")/; requirefunction(require){return require;}',
+  'ignore variable': 'require("a" + b);function require(require){return require;}'
 };
 var results = {
   'normal': 3,
@@ -36,7 +36,7 @@ Object.keys(tests).forEach(function (key){
   suite.add('cmd-deps: ' + key, function (){
     return cdeps(s).length === results[key];
   }).add('detective: ' + key, function (){
-    return cdeps(s).length === results[key];
+    return detective(s).length === results[key];
   })
     // add listeners
     .on('cycle', function (event){
